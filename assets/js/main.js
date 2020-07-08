468d586fcedbe5d9c0fe33265c737a47c078edbe
Daniel-Rauh <daniel.rauh@gmail.com>
2020-07-08 09:01:37 +0200

    Press button to win money git remote add origin https://github.com/Daniel-Rauh/Win_Money.git git push -u origin master !

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+function win() {
+    document.getElementById("winner").innerHTML="Congratulations! You won 1cent!"
+}
+function move() {
+    document.getElementById("run").style.transform="translate(180px, 20px)"
+}
+function move2() {
+    document.getElementById("run").style.transform="translate(130px, 350px)"
+}
+function move3() {
+    document.getElementById("run").style.transform="translate(1330px, 150px)"
+}
+function move4() {
+    document.getElementById("run").style.transform="translate(1130px, 550px)"
+}
+function move5() {
+    document.getElementById("run").style.transform="translate(2px, 2px)"
+}
